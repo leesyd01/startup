d7570bc250cbbe0bc9073121f1254c7e62b3f588
leesyd01 <leesyd01@byu.edu>
2025-11-05 21:42:42 -0700

    added base

--- service/index.js
+++ service/index.js
@@ -1,2 +1,15 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import bcrypt from 'bcryptjs';
+import { v4 as uuidv4 } from 'uuid';
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 app.use(express.static('public'));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`HomeQuest service running on port ${port}`);
+});
